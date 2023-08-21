86c651fa64db5a5c8bb621192e441c97ec597671
Ben Davies <bens.public@gmail.com>
2023-08-21 18:15:05 +1000

    Add command line parser & filter to show only modified repos.

--- vcstat/vcstat.py
+++ vcstat/vcstat.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import argparse
 from git import Repo  # from 'GitPython' pkg
 
 
@@ -14,7 +15,14 @@ CLEAN = f'{GREEN}clean\033{DEFAULT}'
 STATUS_TEMPLATE = '\033[1;37m{path}\033[0m ({dirty}) \033[0;33m{branch}\033[0m'
 
 
-def print_git_status(repo_path, padding_size=0):
+# FIXME: change this to format_status() & return a string for printing
+# TODO: add bash colours to status output?
+def print_git_status(repo_path,
+                     padding_size=0,
+                     show_git_status=False,
+                     show_dirty_only=False):
+    """TODO"""
+    show_clean_repo = not show_dirty_only  # reversed flag for readability
     base_name = os.path.basename(repo_path)
     repo = Repo(repo_path)
 
@@ -23,21 +31,50 @@ def print_git_status(repo_path, padding_size=0):
              'branch': repo.head.ref.name}
 
     tmp = STATUS_TEMPLATE.format(**stats)
-    print((padding_size - len(base_name)) * ' ', tmp)
+
+    if show_git_status:
+        # show long form output for git status
+        status_text = repo.git.status("--short")
+
+        if status_text:
+            # show firty repos: modified & untracked files in status_text
+            out = "\n".join(f"{padding_size * ' '}{line}" for line in status_text.split('\n'))
+            print(f"{tmp}\n{out}\n")
+        else:
+            if show_clean_repo:
+                print(f"{tmp}\n")
+
+    else:
+        # ignore git status, show short form summary of repo status
+        if show_clean_repo or repo.is_dirty():
+            pad = (padding_size - len(base_name))
+            print(pad * ' ', tmp)
 
 
-git_projects = []
-search_dirs = sys.argv[1:] or ['.']
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Show git status for multiple repositories")
+    parser.add_argument("dirs", default=[os.getcwd()], nargs='+',
+                        help="Set root search directories")
 
-for _dir in search_dirs:
-    for root, dirs, _ in os.walk(_dir):
-        for d in dirs:
-            if d == '.git':
+    parser.add_argument("-s", "--status", default=False, action="store_true",
+                        help="Show git status")
+
+    parser.add_argument("-d", "--dirty-only", default=False, action="store_true",
+                        help="Show only dirty/modified repos (inc untracked files)")
+
+    args = parser.parse_args()
+    git_projects = []
+
+    for _dir in args.dirs:
+        for root, dirs, _ in os.walk(_dir):
+            if ".git" in dirs:
                 git_projects.append(root)
 
-if git_projects:
-    print('Git:')
-    longest = max([len(os.path.basename(e)) for e in git_projects])
-    for p in sorted(git_projects):
-        print_git_status(p, longest)
-    print()
+    if git_projects:
+        print('Git:')
+        longest = max([len(os.path.basename(e)) for e in git_projects])
+
+        for p in sorted(git_projects):
+            print_git_status(p, 2 if args.status else longest, args.status, args.dirty_only)
+    else:
+        sys.exit("No repositories found")
